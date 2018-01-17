2fb8b90ba1ebef33a9b80b9267853aa58b6520f2
juunini <juuni.k.qr@gmail.com>
2018-01-17 23:08:57 +0900

    Add files via upload

--- hadoop/js/spa.js
+++ hadoop/js/spa.js
@@ -1,26 +1,26 @@
-var Spa = /** @class */ (function () {
-    function Spa() {
-    }
-    Spa.prototype.header = function () {
-        var xhttp = new XMLHttpRequest;
-        var header = document.querySelector("header")[0];
-        xhttp.onreadystatechange = function () {
-            header.innerHTML = xhttp.responseText;
-        };
-        xhttp.open("GET", "./header.html", true);
-        xhttp.send();
+function header() {
+    var _this = this;
+    var xhttp = new XMLHttpRequest;
+    var header = document.querySelector("header")[0];
+    xhttp.onreadystatechange = function () {
+        if (_this.readyState == 4 && _this.status == 200) {
+            header.innerHTML = _this.responseText;
+        }
     };
-    Spa.prototype.side = function () {
-        var xhttp = new XMLHttpRequest;
-        var header = document.querySelector("aside")[0];
-        xhttp.onreadystatechange = function () {
-            header.innerHTML = xhttp.responseText;
-        };
-        xhttp.open("GET", "./side.html", true);
-        xhttp.send();
+    xhttp.open("GET", "header.html", true);
+    xhttp.send();
+}
+function side() {
+    var _this = this;
+    var xhttp = new XMLHttpRequest;
+    var header = document.querySelector("aside")[0];
+    xhttp.onreadystatechange = function () {
+        if (_this.readyState == 4 && _this.status == 200) {
+            header.innerHTML = _this.responseText;
+        }
     };
-    return Spa;
-}());
-var spa = new Spa();
-spa.header();
-spa.side();
+    xhttp.open("GET", "side.html", true);
+    xhttp.send();
+}
+header();
+side();
--- hadoop/js/spa.ts
+++ hadoop/js/spa.ts
@@ -1,27 +1,26 @@
-class Spa {
-
-    public header() {
-        let xhttp = new XMLHttpRequest;
-        let header = document.querySelector("header")[0];
-        xhttp.onreadystatechange = () => {
-            header.innerHTML = xhttp.responseText;
+function header() {
+    let xhttp = new XMLHttpRequest;
+    let header = document.querySelector("header")[0];
+    xhttp.onreadystatechange = () => {
+        if (this.readyState == 4 && this.status == 200) {
+            header.innerHTML = this.responseText;
         }
-        xhttp.open("GET", "./header.html", true);
-        xhttp.send();
     }
-
-    public side(){
-        let xhttp = new XMLHttpRequest;
-        let header = document.querySelector("aside")[0];
-        xhttp.onreadystatechange = () => {
-            header.innerHTML = xhttp.responseText;
-        }
-        xhttp.open("GET", "./side.html", true);
-        xhttp.send();
-    }
-
+    xhttp.open("GET", "header.html", true);
+    xhttp.send();
 }
 
-const spa = new Spa();
-spa.header();
-spa.side();
+function side() {
+    let xhttp = new XMLHttpRequest;
+    let header = document.querySelector("aside")[0];
+    xhttp.onreadystatechange = () => {
+        if (this.readyState == 4 && this.status == 200) {
+            header.innerHTML = this.responseText;
+        }
+    }
+    xhttp.open("GET", "side.html", true);
+    xhttp.send();
+}
+
+header();
+side();
