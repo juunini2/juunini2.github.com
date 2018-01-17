fbd867d573854f1e33db3e461d6a0df8bd736788
juunini <juuni.k.qr@gmail.com>
2018-01-17 23:36:56 +0900

    Add files via upload

--- hadoop/js/spa.js
+++ hadoop/js/spa.js
@@ -1,18 +1,26 @@
-var _this = this;
-var xhttp = new XMLHttpRequest();
-var header = document.querySelector("header");
-var side = document.querySelector("aside");
-xhttp.onreadystatechange = function () {
-    if (_this.readyState == 4 && _this.status == 200) {
-        header.innerHTML = _this.responseText;
-    }
-};
-xhttp.open("GET", "header.html", true);
-xhttp.send();
-xhttp.onreadystatechange = function () {
-    if (_this.readyState == 4 && _this.status == 200) {
-        side.innerHTML = _this.responseText;
-    }
-};
-xhttp.open("GET", "side.html", true);
-xhttp.send();
+function header() {
+    var _this = this;
+    var xhttp = new XMLHttpRequest();
+    var header = document.querySelector("header");
+    xhttp.onreadystatechange = function () {
+        if (_this.readyState == 4 && _this.status == 200) {
+            header.innerHTML = _this.responseText;
+        }
+    };
+    xhttp.open("GET", "header.html", true);
+    xhttp.send();
+}
+function side() {
+    var _this = this;
+    var xhttp = new XMLHttpRequest();
+    var header = document.querySelector("aside");
+    xhttp.onreadystatechange = function () {
+        if (_this.readyState == 4 && _this.status == 200) {
+            header.innerHTML = _this.responseText;
+        }
+    };
+    xhttp.open("GET", "side.html", true);
+    xhttp.send();
+}
+header();
+side();
--- hadoop/js/spa.ts
+++ hadoop/js/spa.ts
@@ -1,19 +1,26 @@
-let xhttp = new XMLHttpRequest();
-let header = document.querySelector("header");
-let side = document.querySelector("aside");
-
-xhttp.onreadystatechange = () => {
-    if (this.readyState == 4 && this.status == 200) {
-        header.innerHTML = this.responseText;
+function header() {
+    let xhttp = new XMLHttpRequest();
+    let header = document.querySelector("header");
+    xhttp.onreadystatechange = () => {
+        if (this.readyState == 4 && this.status == 200) {
+            header.innerHTML = this.responseText;
+        }
     }
+    xhttp.open("GET", "header.html", true);
+    xhttp.send();
 }
-xhttp.open("GET", "header.html", true);
-xhttp.send();
 
-xhttp.onreadystatechange = () => {
-    if (this.readyState == 4 && this.status == 200) {
-        side.innerHTML = this.responseText;
+function side() {
+    let xhttp = new XMLHttpRequest();
+    let header = document.querySelector("aside");
+    xhttp.onreadystatechange = () => {
+        if (this.readyState == 4 && this.status == 200) {
+            header.innerHTML = this.responseText;
+        }
     }
+    xhttp.open("GET", "side.html", true);
+    xhttp.send();
 }
-xhttp.open("GET", "side.html", true);
-xhttp.send();
+
+header();
+side();
