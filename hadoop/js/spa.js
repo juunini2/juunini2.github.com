0c26e45f4c8f921dca4c18d289982cafd224dbe9
juunini <juuni.k.qr@gmail.com>
2018-01-17 23:33:14 +0900

    Add files via upload

--- hadoop/js/spa.js
+++ hadoop/js/spa.js
@@ -1,21 +1,18 @@
-function spa() {
-    var _this = this;
-    var xhttp = new XMLHttpRequest;
-    var header = document.querySelector("header");
-    var side = document.querySelector("aside");
-    xhttp.onreadystatechange = function () {
-        if (_this.readyState == 4 && _this.status == 200) {
-            header.innerHTML = _this.responseText;
-        }
-    };
-    xhttp.open("GET", "header.html", true);
-    xhttp.send();
-    xhttp.onreadystatechange = function () {
-        if (_this.readyState == 4 && _this.status == 200) {
-            side.innerHTML = _this.responseText;
-        }
-    };
-    xhttp.open("GET", "side.html", true);
-    xhttp.send();
-}
-spa();
+var _this = this;
+var xhttp = new XMLHttpRequest();
+var header = document.querySelector("header");
+var side = document.querySelector("aside");
+xhttp.onreadystatechange = function () {
+    if (_this.readyState == 4 && _this.status == 200) {
+        header.innerHTML = _this.responseText;
+    }
+};
+xhttp.open("GET", "header.html", true);
+xhttp.send();
+xhttp.onreadystatechange = function () {
+    if (_this.readyState == 4 && _this.status == 200) {
+        side.innerHTML = _this.responseText;
+    }
+};
+xhttp.open("GET", "side.html", true);
+xhttp.send();
--- hadoop/js/spa.ts
+++ hadoop/js/spa.ts
@@ -1,23 +1,19 @@
-function spa() {
-    let xhttp = new XMLHttpRequest;
-    let header = document.querySelector("header");
-    let side = document.querySelector("aside");
+let xhttp = new XMLHttpRequest();
+let header = document.querySelector("header");
+let side = document.querySelector("aside");
 
-    xhttp.onreadystatechange = () => {
-        if (this.readyState == 4 && this.status == 200) {
-            header.innerHTML = this.responseText;
-        }
+xhttp.onreadystatechange = () => {
+    if (this.readyState == 4 && this.status == 200) {
+        header.innerHTML = this.responseText;
     }
-    xhttp.open("GET", "header.html", true);
-    xhttp.send();
-
-    xhttp.onreadystatechange = () => {
-        if (this.readyState == 4 && this.status == 200) {
-            side.innerHTML = this.responseText;
-        }
-    }
-    xhttp.open("GET", "side.html", true);
-    xhttp.send();
 }
+xhttp.open("GET", "header.html", true);
+xhttp.send();
 
-spa();
+xhttp.onreadystatechange = () => {
+    if (this.readyState == 4 && this.status == 200) {
+        side.innerHTML = this.responseText;
+    }
+}
+xhttp.open("GET", "side.html", true);
+xhttp.send();
