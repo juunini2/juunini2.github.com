0a320917550ec6c5127f43b1f81fd983facf7138
juunini <juuni.k.qr@gmail.com>
2018-02-10 20:42:09 +0900

    Add files via upload

--- pi/js/denied.js
+++ pi/js/denied.js
@@ -3,7 +3,7 @@ function Denied() {
     if ((navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf("msie") != -1)) {
         document.querySelector("welcome").innerHTML = "You Need Modern Browser.<br><a href='https://www.google.co.kr/chrome/'><img src='/pi/chrome.png' alt=''></a><a href='https://www.mozilla.org/ko/firefox/new/'><img src='/pi/firefox.png' alt=''></a><a href='http://www.opera.com/ko'><img src='/pi/opera.png' alt=''></a>"
     } else {
-        document.querySelector("welcome").innerHTML = "Welcome!"
+        document.querySelector("welcome").innerHTML = "Welcome!";
         document.querySelector("welcome").classList.on("done");
     }
 }
