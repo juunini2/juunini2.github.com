de62d08b09299ffcc5f4b4eb0f52930528845bd7
juunini <juuni.k.qr@gmail.com>
2018-02-10 21:26:49 +0900

    Add files via upload

--- pi/js/denied.js
+++ pi/js/denied.js
@@ -1,11 +1,13 @@
-function Denied() {
+setTimeout(function(){
     const agent = navigator.userAgent.toLowerCase();
+    const welcome = document.querySelector("welcome");
     if ((navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf("msie") != -1)) {
-        document.querySelector("welcome").innerHTML = "You Need Modern Browser.<br><a href='https://www.google.co.kr/chrome/'><img src='/pi/chrome.png' alt=''></a><a href='https://www.mozilla.org/ko/firefox/new/'><img src='/pi/firefox.png' alt=''></a><a href='http://www.opera.com/ko'><img src='/pi/opera.png' alt=''></a>"
+        welcome.innerHTML = "You Need Modern Browser.<br><a href='https://www.google.co.kr/chrome/'><img src='/pi/chrome.png' alt=''></a><a href='https://www.mozilla.org/ko/firefox/new/'><img src='/pi/firefox.png' alt=''></a><a href='http://www.opera.com/ko'><img src='/pi/opera.png' alt=''></a>"
     } else {
-        document.querySelector("welcome").innerHTML = "Welcome!";
-        document.querySelector("welcome").classList.on("done");
+        welcome.innerHTML = "Welcome!";
+        welcome.classList.add("done");
+        setTimeout(function(){
+            welcome.style.display = "none";
+        }, 2000)
     }
-}
-
-setTimeout(Denied(), 3000);
+}, 3000);
